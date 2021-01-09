9878c4611cda7e02adbd639edb0b8062c07cd5d6
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-01-09 00:28:36 -0300

    stockQuote - initial commit and comments added

---
+++ stockQuote.sh
@@ -0,0 +1,56 @@
+#! /usr/bin/env bash
+
+# stockQuote.sh - Displays the value of the closing, opening, minimum 
+# and maximum share price
+#
+# Site:       
+# Author:      Joao Lopes
+# Maintainer: Joao Lopes
+#
+# ------------------------------------------------------------------------ #
+#  This program quotes the opening, closing, minimum and maximum value of 
+#  a given stock on a given stock exchange in the world based on the API of
+#  marketstack (https://marketstack.com/).
+#  For using it, export the variable API_TOKEN, you can get a free token
+#  for 1000 (one thousand) requests per month at their website.
+#
+#  Examples:
+#      $ export API_TOKEN=YOUR_API_TOKEN_HERE
+#      $ ./stockQuote.sh -e [exchange] -t [ticker]
+#      $ ./stockQuote.sh -e BVMF -t PETR3
+#      
+#      In this example, the price of the PETR3 stock on the BVMF stock exchange 
+#      (Brazilian stock exchange) will be displayed.
+#
+# ------------------------------------------------------------------------ #
+# Changelog
+#
+#   v1.0 08/01/2021, Joao:
+#       - Initial program
+#       - display the requested stock price
+#
+# ------------------------------------------------------------------------ #
+# Tested with:
+#   bash 5.0.17
+# ------------------------------------------------------------------------ #
+
+############################################################################
+# VARIABLES
+############################################################################
+
+
+############################################################################
+# FUNCTIONS
+############################################################################
+
+
+############################################################################
+# TESTS
+############################################################################
+
+
+############################################################################
+# EXECUTION
+############################################################################
+
+
