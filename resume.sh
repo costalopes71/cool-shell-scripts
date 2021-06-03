aa377c8d2e9e3f6b45148585806333612a74371a
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-06-03 19:30:36 -0300

    resume: vi and top commands

--- resume.sh
+++ resume.sh
@@ -36,6 +36,17 @@ BLUE="\033[34;1m"
 GREEN="\033[32;1m"
 YELLOW="\033[33;1m"
 
+HELP="This program is intended to facilitate the resumes of my most used commands.
+
+Commands: vi, top
+Subcommands: vi delete, vi alter, vi cut, vi copy, vi paste, vi navegate, vi insert
+
+Usage example:
+  $ resume vi
+	$ resume top
+	$ resume vi delete
+"
+
 CMD_VI_NAVEGATION="Navegacao:
 j --> move o cursor uma linha para baixo
 k --> move o cursor uma linha para cima
@@ -53,7 +64,7 @@ M --> move o cursor para a linha do meio da tela
 L --> move o cursor para a ultima linha da tela
 H --> move o cursor para a primeira linha da tela"
 
-CMD_VI_INSERT_TEXT="Digitacao:
+CMD_VI_INSERT_TEXT="Insercao/Digitacao:
 i --> insere texto antes do cursor
 I --> insere texto no comeco da linha 
 a --> insere texto depois do cursor
@@ -70,16 +81,16 @@ C --> faz o replace da linha toda pelo texto que irei escrever
 cc --> nao entendi a diferenca do de cima
 ncc --> faz o replace de N linhas comecando pela linha atual"
 
-CMD_VI_DELETE="Delecao:
-x --> deleta um caracter 
-Nx --> deleta N caracteres
-dw --> deleta a palavra toda
-dNw --> deleta N palavras
-D --> deleta o restante da linha a comecar do cursor
-dd --> deleta a linha toda
-Ndd --> deleta N linhas"
+CMD_VI_CUT="Recortar:
+x   --> recorta/deleta um caracter 
+Nx  --> recorta/deleta N caracteres
+dw  --> recorta/deleta a palavra toda
+dNw --> recorta/deleta N palavras
+D   --> recorta/deleta o restante da linha a comecar do cursor
+dd  --> recorta/deleta a linha toda
+Ndd --> recorta/deleta N linhas"
 
-CMD_VI_CUT="Recortar/Colar:
+CMD_VI_COPY_PASTE="Copiar/Colar:
 yy --> copia a linha
 Nyy --> copia N linhas
 p --> cola"
@@ -92,18 +103,49 @@ J --> junta as linhas (a do cursor com a debaixo)
 << --> shifta a linha para a esquerda
 "
 
+CMD_VI_ALL="$CMD_VI_NAVEGATION\n\n$CMD_VI_INSERT_TEXT\n\n$CMD_VI_ALTER\n\n$CMD_VI_CUT\n\n$CMD_VI_COPY_PASTE"
+
+CMD_TOP_ALL="m --> exibe o valor de memoria usada usando uma barra (apertar mais 1x exibe a barra com preenchimento e mais uma vez tira a linha de memoria)
+t --> exibe o valor de cpu em uso usando uma barra (apertar mais 1x exibe a barra com preenchimento e mais uma vez tira a linha da cpu)
+P --> ordena por CPU
+M --> ordena por memoria
+T --> ordena por Tempo
+N --> ordena por PID
+c --> mostra o comando todo executado
+n --> quantidade de linhas a serem exibidas
+k --> matar um PID
+u --> permite escolher os processos de um determinado usuario
+1 --> mostra os valores de cada CPU
+"
+
+########################################################
+# FUNCTIONS
 ########################################################
 
-if [ "$1" = "vi" ]; then
-	
-	case "$2" in
-		navegate) echo "$CMD_VI_NAVEGATION" && exit 0       ;;
-		insert) echo "$CMD_VI_INSERT_TEXT" && exit 0        ;;
-		alter) echo "$CMD_VI_ALTER" && exit 0               ;;
-		delete) echo "$CMD_VI_DELETE" && exit 0             ;;
-		cut) echo "$CMD_VI_CUT" && exit 0                   ;;
-		*) echo "help command" && exit 0                    ;;
+printViCommands() {
+	case "$1" in
+		navegate) echo "$CMD_VI_NAVEGATION" && exit 0   ;;
+		insert) echo "$CMD_VI_INSERT_TEXT" && exit 0    ;;
+		alter) echo "$CMD_VI_ALTER" && exit 0           ;;
+		delete) echo "$CMD_VI_CUT" && exit 0            ;;
+		cut) echo "$CMD_VI_CUT" && exit 0               ;;
+		copy) echo "$CMD_VI_COPY_PASTE" && exit 0       ;;
+		paste) echo "$CMD_VI_COPY_PASTE" && exit 0      ;;
+		*) echo -e "$CMD_VI_ALL" && exit 0              ;;
 	esac
+}
 
-fi
+printTopCommands() {
+	echo "$CMD_TOP_ALL"
+}
+
+########################################################
+# EXECUTION
+########################################################
+
+case "$1" in
+	vi) printViCommands $2 && exit 0       ;;
+	top) printTopCommands && exit 0        ;;
+	*) echo "$HELP" && exit 0              ;;
+esac
 
